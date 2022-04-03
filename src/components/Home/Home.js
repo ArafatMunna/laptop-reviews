b6497c229a600f1cbc730ea67c9ec179de74f73f
ArafatMunna <arafatmunna000@gmail.com>
2022-04-04 00:28:12 +0600

    create reviews json and custom hooks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,16 @@
 import React from "react";
-import images from "../../images/apple.jpg";
+import useReviews from "../../hooks/useReviews";
+import LaptopDetails from "../LaptopDetails/LaptopDetails";
 
 const Home = () => {
+    const [reviews, setReviews] = useReviews();
+
     return (
         <div className="mt-4 container">
-            <h1 className="text-primary text-center">Laptop Reviews</h1>
-            <div className="row my-5">
-                <div className="col-12 col-md-6">
-                    <h2>MacBook Air</h2>
-                    <p>
-                        The MacBook Air is the best MacBook for most people.
-                        This latest model demonstrates the capabilities of
-                        Apple's M1 chip, which transforms the Air from an
-                        underpowered entry-level option to one of the fastest
-                        laptops on the market, period. It's a complete
-                        turnaround for a laptop that was just barely getting by
-                        each year.{" "}
-                    </p>
-                    <p>
-                        <b>Specification:</b> CPU: M1GPU: M1 (integrated)RAM:
-                        8GB/16GBStorage: 256GB/512GB/1TB/2TBDisplay: 13.3
-                        inches, 2560 x 1600 pixelsSize: 12 x 8.4 x 0.6
-                        inchesWeight: 2.8 pounds
-                    </p>
-                </div>
-                <div className="col-12 col-md-6">
-                    <div className="w-75 mx-auto">
-                        <img className="img-fluid rounded-3" src={images} alt="" />
-                    </div>
-                </div>
-            </div>
+            <h1 className="text-primary text-center fw-bold">Laptop Reviews</h1>
+            <LaptopDetails />
+
+            <h2 className="text-center">Customer Reviews (3)</h2>
         </div>
     );
 };

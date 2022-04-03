700200c04c25d0e72dc9cb23c5a3811244d5f115
ArafatMunna <arafatmunna000@gmail.com>
2022-04-04 04:36:39 +0600

    netlify 404 error solve and live site link added in readme file

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import notFoundImg from "../../images/not-found.jpg";
 
 const NotFound = () => {
+    const navigate = useNavigate();
     return (
         <div className="container mt-5 text-center">
             <div>
-                <h1>Page Not Found!!!</h1>
-                <h2>404</h2>
+                <img className="img-fluid" src={notFoundImg} alt="" />
             </div>
+            <button onClick={()=> navigate("/")} className="btn btn-primary">Back to Home</button>
         </div>
     );
 };

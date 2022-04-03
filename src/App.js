ddcf53ed8792ab613cb7a632b5d6faa5043b158e
ArafatMunna <arafatmunna000@gmail.com>
2022-04-04 01:22:54 +0600

    not found page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 import Reviews from "./components/Reviews/Reviews";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/reviews" element={<Reviews />} />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
     );
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <div className="container mt-5 text-center">
+            <div>
+                <h1>Page Not Found!!!</h1>
+                <h2>404</h2>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;

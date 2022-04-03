511431cea59b722d9423e401958f6993faa2262c
ArafatMunna <arafatmunna000@gmail.com>
2022-04-03 23:05:00 +0600

    navbar added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <div className="bg-primary p-3 fs-5">
+            <div className="container d-flex justify-content-between">
+                <Link className="text-white text-decoration-none" to="/">
+                    <h3>Laptop Reviews</h3>
+                </Link>
+                <div>
+                    <NavLink
+                        className="text-white text-decoration-none me-4"
+                        to="/"
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink
+                        className="text-white text-decoration-none me-4"
+                        to="/reviews"
+                    >
+                        Reviews
+                    </NavLink>
+                    <NavLink
+                        className="text-white text-decoration-none me-4"
+                        to="/dashboard"
+                    >
+                        Dashboard
+                    </NavLink>
+                    <NavLink
+                        className="text-white text-decoration-none me-4"
+                        to="/blogs"
+                    >
+                        Blogs
+                    </NavLink>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
